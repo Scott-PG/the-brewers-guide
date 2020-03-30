490bb1583280ed964140625d7050a3b4a464fe90
scottpg <scottpgriffith@gmail.com>
2020-03-30 13:56:35 -0400

    Built out the app structure

---
+++ brewers-guide-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Header Placeholder</h1>
+        <nav></nav>
+      </header>
+      <main></main>
+      <footer>
+        <p>Designed by ScottPG</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
